0a542baab4ec7600065b0e0d0f5d3eff643d9be8
Alexandra Bashkirova <s.bshkrva@gmail.com>
2020-04-03 22:01:13 +0700

    add log4j2 - R3.1, R3.2
    update build and run scripts
    add log config to src/resources

--- src/Main.kt
+++ src/Main.kt
@@ -4,33 +4,75 @@ import mock.SessionMock
 import mock.UsersMock
 import services.*
 import kotlin.system.exitProcess
+import org.apache.logging.log4j.kotlin.logger
 
-fun main(args: Array<String>) {
-    println("Инициализация")
-    val authenticationService = AuthenticationService(UsersMock().users)
-    val authorizationService = AuthorizationService(ResoursesMock().resources)
-    val accountingService = AccountingService(SessionMock.session)
-    val businessLogic = BusinessLogic(authenticationService, authorizationService, accountingService)
-    val cmdServise = CmdServise(args)
-    val cmd = cmdServise.parse()
-    var status = SUCCESS
-    if (cmdServise.isAuthenticationNeeded()) {
-        println("Аутентифицируем пользователя ${cmd.login!!} с паролем ${cmd.password!!}")
-        status = businessLogic.authentication(cmd.login!!, cmd.password!!)
+object Main {
+    private val logger = logger()
 
-        println("Результат шага: ${status.name}\n---------")
+    @JvmStatic
+    fun main(args: Array<String>) {
+        logger.info { "Инициализация: ${args.joinToString(" ")}" }
+
+        val authenticationService = AuthenticationService(UsersMock().users)
+        val authorizationService = AuthorizationService(ResoursesMock().resources)
+        val accountingService = AccountingService(SessionMock.session)
+        val businessLogic = BusinessLogic(
+                authenticationService,
+                authorizationService,
+                accountingService,
+                logger
+        )
+        val cmdServise = CmdServise(args)
+        val cmd = cmdServise.parse()
+        var status = SUCCESS
+        if (cmdServise.isAuthenticationNeeded()) {
+            logger.info {
+                "Попытка аутентифиции пользователя: " +
+                        "${cmd.login!!} с паролем ${cmd.password!!}"
+            }
+            status = businessLogic.authentication(cmd.login!!, cmd.password!!)
+
+            logger.info {
+                "Результат шага: ${status.name}"
+            }
+        }
+        if (status == SUCCESS && cmdServise.isAuthorizationNeeded()) {
+            logger.info {
+                "Попытка авторизации пользователя к ресурсу: " +
+                        "${cmd.resource!!} c ролью - ${cmd.role!!}"
+            }
+
+            status = businessLogic.authorization(cmd.login!!, cmd.role!!, cmd.resource!!)
+
+            logger.info {
+                "Результат шага: ${status.name}"
+            }
+        }
+        if (status == SUCCESS && cmdServise.isAccountingNeeded()) {
+            logger.info {
+                "Попытка аккаунтинга: к ресурсу ${cmd.dateStart!!} - ${cmd.dateEnd!!}, " +
+                        "потребляемый объем ${cmd.volume!!}"
+            }
+
+            status = businessLogic.accounting(
+                    cmd.login!!,
+                    cmd.resource!!,
+                    cmd.role!!,
+                    cmd.dateStart!!,
+                    cmd.dateEnd!!,
+                    cmd.volume!!
+            )
+
+            logger.info {
+                "Результат шага: ${status.name}"
+            }
+        }
+
+        logger.info {
+            "Завершение программы с кодом: ${status.codeNumber}" +
+                    "\n---------"
+        }
+
+        exitProcess(status.codeNumber)
     }
-    if (status == SUCCESS && cmdServise.isAuthorizationNeeded()) {
-        println("Авторизируем пользователя к ресурсу ${cmd.resource!!} c ролью - ${cmd.role!!}")
-        status = businessLogic.authorization(cmd.login!!, cmd.role!!, cmd.resource!!)
-        println("Результат шага: ${status.name}\n---------")
-    }
-    if (status == SUCCESS && cmdServise.isAccountingNeeded()) {
-        println("Попытка аккаунтинга к ресурсу ${cmd.dateStart!!} - ${cmd.dateEnd!!}, потребляемы объем ${cmd.volume!!}")
-        status = businessLogic.accounting(cmd.login!!, cmd.resource!!, cmd.role!!, cmd.dateStart!!, cmd.dateEnd!!, cmd.volume!!)
-        println("Результат шага: ${status.name}\n---------")
-    }
-    println("Завершение программы с кодом ${status.codeNumber}")
-    exitProcess(status.codeNumber)
 }
-
--- src/services/BusinessLogic.kt
+++ src/services/BusinessLogic.kt
@@ -4,59 +4,63 @@ import enum.ExitCode
 import enum.ExitCode.*
 import enum.Roles
 import enum.findRoles
+import org.apache.logging.log4j.kotlin.KotlinLogger
 
 class BusinessLogic(
         private val authenticationService: AuthenticationService,
         private val authorizationService: AuthorizationService,
-        private val accountingService: AccountingService
+        private val accountingService: AccountingService,
+        private val logger: KotlinLogger
 ) {
     fun authentication(login: String, pass: String): ExitCode {
         val isLoginValidated: Boolean = authenticationService.validateLogin(login)
-        println("Результат валидации логина: $isLoginValidated")
+
+        logger.info { "Результат валидации логина: $isLoginValidated" }
+
         val isLoginExist: Boolean
         val isPasswordVerificated: Boolean
         if (isLoginValidated) {
             isLoginExist = authenticationService.findUserLogin(login)
-            println("Результат поиска пользователя: $isLoginExist")
+            logger.info { "Результат поиска пользователя: $isLoginExist" }
         } else {
-            println("Невалидный формат логина")
+            logger.error { "Невалидный формат логина" }
             return INVALID_LOGIN
         }
         if (isLoginExist) {
             isPasswordVerificated = authenticationService.verificationPassword(login, pass)
-            println("Результат верификации пользователя с паролем: $isPasswordVerificated")
+            logger.info { "Результат верификации пользователя с паролем: $isPasswordVerificated" }
         } else {
-            println("Неизвестный пользователь")
+            logger.error { "Неизвестный пользователь" }
             return UNKNOWN_LOGIN
         }
         return if (isPasswordVerificated) {
             SUCCESS
         } else {
-            println("Неправильный пароль")
+            logger.error { "Неправильный пароль" }
             INVALID_PASSWORD
         }
     }
 
     fun authorization(login: String, role: String, resource: String): ExitCode {
         val isRoleExist = findRoles(role)
-        println("Результат валидации роли: $isRoleExist")
+        logger.info { "Результат валидации роли: $isRoleExist" }
         val isChildAccessExist: Boolean
         var isParentAccessExist = false
         if (isRoleExist) {
             isChildAccessExist = authorizationService.checkResourceAccess(login, resource, role)
         } else {
-            print("Не известная роль")
+            logger.error { "Не известная роль" }
             return UNKNOWN_ROLE
         }
         if (!isChildAccessExist) {
             isParentAccessExist = authorizationService.isParentHaveAccess(resource, login, role)
         }
         val isAccessExist = isChildAccessExist || isParentAccessExist
-        println("Результат верификации доступа к ресурсу: $isAccessExist")
+        logger.info { "Результат верификации доступа к ресурсу: $isAccessExist" }
         return if (isAccessExist) {
             SUCCESS
         } else {
-            println("Нет доступа")
+            logger.error { "Нет доступа" }
             FORBIDDEN
         }
     }
@@ -65,14 +69,14 @@ class BusinessLogic(
         val dateStarted = accountingService.parseDate(ds)
         val dateEnd = accountingService.parseDate(de)
         val isDateValided = dateStarted != null && dateEnd != null && dateStarted.compareTo(dateEnd) == -1
-        println("Результат валидации даты начала и конца: $isDateValided")
+        logger.info { "Результат валидации даты начала и конца: $isDateValided" }
         val isVolumeValided = accountingService.validateVolume(vol)
-        println("Результат валидации объема: $isVolumeValided")
+        logger.info { "Результат валидации объема: $isVolumeValided" }
         return if (isDateValided && isVolumeValided) {
             accountingService.addNewSession(login, resource, Roles.valueOf(role), ds, de, vol.toInt())
             SUCCESS
         } else {
-            println("Неверная активность")
+            logger.error { "Неверная активность" }
             INCORRECT_ACTIVITY
         }
     }
