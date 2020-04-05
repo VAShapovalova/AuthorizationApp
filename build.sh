f41a94688f36577febc8987674a49b0156acb9ac
Vera Shapovalova <17Ametista17@gmail.com>
2020-04-05 22:43:57 +0700

    Update scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 ##!/bin/bash
 
-kotlinc -d out/AppCli.jar -include-runtime src -cp lib/kotlinx-cli-0.2.1.jar:lib/h2-1.4.200.jar
+kotlinc -d out/AppCli.jar -include-runtime src -cp lib/kotlinx-cli-0.2.1.jar:lib/h2-1.4.200.jar:lib/flyway-core-6.3.2.jar
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 ##!/bin/bash
 
-java -classpath "out/AppCli.jar:lib/kotlinx-cli-0.2.1.jar:lib/h2-1.4.200.jar" MainKt $@
+java -classpath "out/AppCli.jar:lib/kotlinx-cli-0.2.1.jar:lib/h2-1.4.200.jar:lib/flyway-core-6.3.2.jar" MainKt $@
