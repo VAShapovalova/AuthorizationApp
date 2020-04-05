89c63d9c02b79e8106ea49638b5dae0e41e8be1d
Vera Shapovalova <17Ametista17@gmail.com>
2020-04-06 01:00:53 +0700

    add flyway migration

--- src/Main.kt
+++ src/Main.kt
@@ -6,34 +6,37 @@ import services.*
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    val dbService = DBService()
-
-    val authenticationService = AuthenticationService(
-            UserDAO(dbService.connection)
-    )
-    val authorizationService = AuthorizationService(ResoursesMock().resources)
-    val accountingService = AccountingService(SessionMock.session)
-    val businessLogic = BusinessLogic(authenticationService, authorizationService, accountingService)
     val cmdServise = CmdServise(args)
     val cmd = cmdServise.parse()
     var status = SUCCESS
-    if (cmdServise.isAuthenticationNeeded()) {
-        status = businessLogic.authentication(cmd.login!!, cmd.password!!)
-    }
-    if (status == SUCCESS && cmdServise.isAuthorizationNeeded()) {
-        status = businessLogic.authorization(cmd.login!!, cmd.role!!, cmd.resource!!)
-    }
-    if (status == SUCCESS && cmdServise.isAccountingNeeded()) {
-        status = businessLogic.accounting(
-                cmd.login!!,
-                cmd.resource!!,
-                cmd.role!!,
-                cmd.dateStart!!,
-                cmd.dateEnd!!,
-                cmd.volume!!
+    val dbService = DBService()
+    dbService.migrate()
+    dbService.connect()
+    if (dbService.connection != null) {
+        val authenticationService = AuthenticationService(
+                UserDAO(dbService.connection!!)
         )
-    }
+        val authorizationService = AuthorizationService(ResoursesMock().resources)
+        val accountingService = AccountingService(SessionMock.session)
+        val businessLogic = BusinessLogic(authenticationService, authorizationService, accountingService)
 
-    dbService.connection.close()
+        if (cmdServise.isAuthenticationNeeded()) {
+            status = businessLogic.authentication(cmd.login!!, cmd.password!!)
+        }
+        if (status == SUCCESS && cmdServise.isAuthorizationNeeded()) {
+            status = businessLogic.authorization(cmd.login!!, cmd.role!!, cmd.resource!!)
+        }
+        if (status == SUCCESS && cmdServise.isAccountingNeeded()) {
+            status = businessLogic.accounting(
+                    cmd.login!!,
+                    cmd.resource!!,
+                    cmd.role!!,
+                    cmd.dateStart!!,
+                    cmd.dateEnd!!,
+                    cmd.volume!!
+            )
+        }
+    }
+    dbService.disconnect()
     exitProcess(status.codeNumber)
 }
--- src/services/DBService.kt
+++ src/services/DBService.kt
@@ -1,16 +1,25 @@
 package services
 
+import org.flywaydb.core.Flyway
 import java.sql.Connection
 import java.sql.DriverManager
 
-class DBService {
-    var connection: Connection
-        private set
+class DBService() {
+    val envUrl: String = System.getenv("DBURL")
+    val envLogin: String = System.getenv("DBLOGIN")
+    val envPass: String = System.getenv("DBPASS")
+    val migrationPath: String = "filesystem:db"
+    var connection: Connection? = null
 
-    init {
-        val envUrl: String = System.getenv("DBURL") ?: "jdbc:h2:./db/AuthorizationApp"
-        val envLogin: String = System.getenv("DBLOGIN") ?: "sa"
-        val envPass: String = System.getenv("DBPASS") ?: ""
-        this.connection = DriverManager.getConnection(envUrl, envLogin, envPass)
+    fun connect() {
+        connection = DriverManager.getConnection(envUrl, envLogin, envPass)
     }
+
+    fun disconnect() {
+        if (connection != null) {
+            connection!!.close()
+        }
+    }
+
+    fun migrate() = Flyway.configure().dataSource(envUrl, envLogin, envPass).locations(migrationPath).load().migrate()
 }
