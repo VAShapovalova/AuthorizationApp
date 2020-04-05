d799314dd68f5caf1b12839e27a8f93e14846fab
Vera Shapovalova <17Ametista17@gmail.com>
2020-04-06 00:59:23 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 ##!/bin/bash
+if [ -z "$DBURL" ]; then
+    export DBURL=jdbc:h2:./db/AuthorizationApp
+fi
+if [ -z "$DBLOGIN" ]; then
+    export DBLOGIN=sa
+fi
+if [ -z "$DBPASS" ]; then
+    export DBPASS=
+fi
 
 java -classpath "out/AppCli.jar:lib/kotlinx-cli-0.2.1.jar:lib/h2-1.4.200.jar:lib/flyway-core-6.3.2.jar" MainKt $@
