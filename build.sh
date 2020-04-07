a6cbebf1ffa31a2a5bda5c5828ef8031374e29fe
ABashkirova <alex.bashkirova@gmail.com>
2020-04-07 22:02:59 +0700

    add rm out dir in build and add env to travis

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 ##!/bin/bash
-
+rm -rf out
+mkdir out
 kotlinc -d out/AppCli.jar -include-runtime src -cp lib/kotlinx-cli-0.2.1.jar:lib/h2-1.4.200.jar:lib/flyway-core-6.3.2.jar:lib/log4j-core-2.13.1.jar:lib/log4j-api-kotlin-1.0.0.jar:lib/log4j-api-2.13.1.jar
