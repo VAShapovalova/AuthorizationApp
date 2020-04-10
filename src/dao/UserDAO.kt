af8b5449668bd9343d583145a5230afe824c3bda
Alexandra Bashkirova <s.bshkrva@gmail.com>
2020-04-10 20:50:04 +0700

    return non-closed statement and result

--- src/dao/UserDAO.kt
+++ src/dao/UserDAO.kt
@@ -11,23 +11,16 @@ class UserDAO(private val dbConnection: Connection) {
             WHERE LOGIN=?;
         """
 
-        var user: User? = null
         val statement = dbConnection.prepareStatement(sql)
-        dbConnection
-        user = statement.use {
-
-            it.setString(1, login)
-            val value = it.executeQuery()
-
-            return when {
-                value.next() -> User(
-                        login = value.getString("LOGIN"),
-                        hash = value.getString("HASH_PASSWORD"),
-                        salt = value.getString("SALT")
-                )
-                else -> null
-            }
+        statement.setString(1, login)
+        val value = statement.executeQuery()
+        return when {
+            value.next() -> User(
+                    login = value.getString("LOGIN"),
+                    hash = value.getString("HASH_PASSWORD"),
+                    salt = value.getString("SALT")
+            )
+            else -> null
         }
-        return user
     }
 }
